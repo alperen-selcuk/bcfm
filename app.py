52ed0a9fa51baa757130b682b01709559a20999f
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2021-09-17 15:08:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import request, Flask
-import json
+from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask_prometheus_metrics import register_metrics
+
 
 app = Flask(__name__)
-@app.route('/bcfm')
+@app.route("/bcfm")
+def index():
+  return "BCFM"
 
-def hello_world():
-  
-  return 'BCFM'
+register_metrics(app, app_version="v0.1.2", app_config="staging")
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
 
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
