5520ca23db326cbcd458250d8c55e65694f842ab
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2021-09-17 17:02:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import request, Flask
-import json
+import random
+import time
+
+from flask import Flask, render_template_string, abort
+from prometheus_client import generate_latest, REGISTRY, Counter, Gauge, Histogram
 
 app = Flask(__name__)
+
+# A counter to count the total number of HTTP requests
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests (count)', ['method', 'endpoint', 'status_code'])
+
+# A gauge (i.e. goes up and down) to monitor the total number of in progress requests
+IN_PROGRESS = Gauge('http_requests_inprogress', 'Number of in progress HTTP requests')
+
+# A histogram to measure the latency of the HTTP requests
+TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seconds)')
+
+
+
+
 @app.route('/bcfm')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def index():
+    return "BCFM"
 
-def hello_world():
-  
-  return 'BCFM GREEN v2'
 
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+@app.route('/metrics')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def metrics():
+    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
+    return generate_latest(REGISTRY)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
