2e229db47b0883351d47395c9ff6493968abc381
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2021-09-17 15:24:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def index():
 register_metrics(app, app_version="v0.1.2", app_config="staging")
 dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
 
-run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
+run_simple(hostname="0.0.0.0", application=dispatcher)
