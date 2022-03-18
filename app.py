760ad854c56cb71ae917a77cc24726cc7b8a9af2
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-03-18 23:19:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
+import os
 from flask import Flask
-from prometheus_client import make_wsgi_app
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
-from flask_prometheus_metrics import register_metrics
-
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "BCFM"
+def hello():
+    return "Hello from GITLAB!"
 
-# provide app's version and deploy environment/config name to set a gauge metric
-register_metrics(app, app_version="v0.1.2", app_config="staging")
-
-# Plug metrics WSGI app to your main app with dispatcher
-dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
-
-run_simple(hostname="localhost", port=5000, application=dispatcher)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
